cf7bc60b935ce5cddd93b24d28d75a79273c8979
NoahNye <qq071855@gmail.com>
2017-10-23 23:52:17 +0800

    OpencvL 1
    读取图像

---
+++ 1.test.cpp
@@ -0,0 +1,15 @@
+
+#include "cv.h"
+#include "highgui.h"
+#include "cxcore.h"
+
+int main()
+{
+	IplImage* src = cvLoadImage("d:\\pic.jpg",1);//创建一个变量赋予图像内容
+	cvNamedWindow("win");						 //创建显示窗口
+	cvShowImage("win", src);					 //显示图像
+	cvWaitKey(0);								 //设置停留时间	
+	cvReleaseImage(&src);						 //释放图像内存	
+	cvDestroyWindow("win");						 //释放窗口内存
+	return 0;
+}
